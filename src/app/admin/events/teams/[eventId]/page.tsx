047bd853dbf1093e7fd7794892b4cfabcaeb6554
Hatim Aqeel <hatimaqeel34@gmail.com>
2025-11-02 21:21:00 +0000

    make the team name font size 20px, but increase the card width so whatev

--- src/app/admin/events/teams/[eventId]/page.tsx
+++ src/app/admin/events/teams/[eventId]/page.tsx
@@ -219,7 +219,7 @@ export default function ManageTeamsPage() {
         </Button>
       </PageHeader>
 
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2">
         {event?.teams?.map(team => {
           const teamIsFull = eventSport && team.players.length >= eventSport.teamSize;
           return (
