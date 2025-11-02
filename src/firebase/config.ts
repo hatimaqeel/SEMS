42d57c31ee6ded22291055aef515cfe08f6f931e
Hatim Aqeel <hatimaqeel34@gmail.com>
2025-11-02 19:24:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4529446714-6c24b",
+  "appId": "1:283599249665:web:c6dd1a04c8b17637817b00",
+  "apiKey": "AIzaSyDECYBoAj_uVtBJlFue-T3yK_oUbj1W5Wg",
+  "authDomain": "studio-4529446714-6c24b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "283599249665"
+};
